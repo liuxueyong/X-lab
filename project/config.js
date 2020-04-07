515948970f5bfc055922b99a4bdae69d8edc25ff
LXY <liu.xueyong@qq.com>
2020-04-08 04:57:47 +0800

    updat

---
+++ project/config.js
@@ -0,0 +1,23 @@
+const UserConfig = {
+
+    SESSION: 'YmRmMDY3ZjQtOWI0MC00NDlkLTljYjItZGFkMGUzMTgwYTA5',
+}
+
+const SignConfig = {
+
+    longitude: '113.2732',
+    latitude: '23.157159',
+
+
+    country: '中国',
+    city: '广州市',
+    district: '白云区',
+    street: '政通路',
+    province: '广东省',
+    township: '景泰街道',
+
+
+    answers: '["0"]',
+}
+
+module.exports = { UserConfig, SignConfig };
